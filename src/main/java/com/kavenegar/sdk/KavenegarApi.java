07627423351ae8985f7c467dbfb73cf88a41c32b
Hadi <Hadi@DESKTOP-UGPISHF>
2017-08-12 10:25:54 +0430

    add token10 and token20 to verifylookup

--- src/main/java/com/kavenegar/sdk/KavenegarApi.java
+++ src/main/java/com/kavenegar/sdk/KavenegarApi.java
@@ -490,7 +490,13 @@ public class KavenegarApi {
         JsonObject json = array.get(0).getAsJsonObject();
         return new SendResult(json);
     }
-
+    
+    public SendResult verifyLookup(String receptor, String token, String token2, String token3, String template,String token10,String token20) throws BaseException {
+        String path = getApiPath("verify/lookup");
+        JsonArray array = execute(path, "receptor", receptor, "token", token, "token2", token2, "token3", token3, "template", template,"token10",token10,"token20",token20).getAsJsonArray();
+        JsonObject json = array.get(0).getAsJsonObject();
+        return new SendResult(json);
+    }
     public SendResult verifyLookup(String receptor, String token, String template) throws BaseException {
         return verifyLookup(receptor, token, "", "", template);
     }
